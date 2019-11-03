b4f99ef8140ebbd89c9734ae426b48586f792b0f
arthurmeireles <48288137+arthurmeireles@users.noreply.github.com>
2019-11-03 01:45:34 -0300

    Avanços nos structs problemas entre strings
    Falta fazer uma comparação certa entre as string, quando eu acertar isso vai resolver todos os problemas

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 struct Cliente{
   char nome[255];
@@ -10,26 +11,27 @@ struct Cliente{
 };
 
 struct Gerente{
-  char nome[11];
   int login;
   char senha;
 };
 
 struct Agencia{
-  Gerente gerente[1];
-  Cliente clientes[100];
+  struct Gerente gerente;
+  struct Cliente clientes[100];
 };
 
 int main(void) {
+
   struct Agencia agencia;
-  struct Gerente gerente;
-  gerente.nome[11] = "Coleguinha";
-  gerente.login = 12345;
-  gerente.senha[]
+  agencia.gerente.login = 12345;
+  agencia.gerente.senha = "batata";
+
+
+
   int contadorClientes = 0; 
 
   while(1>0){
-    printf("Deseja entrar como Gerente ou como Cliente? \nDigite [1] para entrar como cliente.\nDigite [2] para entrar como cliente.");
+    printf("Deseja entrar como Gerente ou como Cliente? \nDigite [1] para entrar como gerente.\nDigite [2] para entrar como cliente.\n");
 
     int entradaUser;
 
@@ -37,32 +39,42 @@ int main(void) {
 
     if(entradaUser == 1){
       
-      printf("Digite seu login");
+      printf("Digite seu login: \n");
       int loginEntrada;
       scanf("%d", &loginEntrada);
         
-        if(loginEntrada == gerente.login){
+        if(loginEntrada == agencia.gerente.login){
 
-          printf("Digite sua senha");
-          char senhaEntrada[6];
+          printf("Digite sua senha: \n");
+
+          char senhaEntrada[7];
           scanf("%s", &senhaEntrada);
 
-          if(senhaEntrada == gerente.senha){
-            printf("----------Menu Gerente--------\n");
-            printf("Digite [1] para Cadastrar um cliente\n");
-            printf("Digite [2] para Excluir um cliente\n");
-            printf("Digite [3] para consultar saldo\n");
+          printf("%s", senhaEntrada);
 
-            int entradaMenuGerente;
-            scanf("%d", &entradaMenuGerente);
+          printf("%d", strcmp(senhaEntrada, agencia.gerente.senha));
+
+
+          // if(strcmp(senhaEntrada, agencia.gerente.senha) == 0){
+          //   printf("----------Menu Gerente--------\n");
+          //   printf("Digite [1] para Cadastrar um cliente\n");
+          //   printf("Digite [2] para Excluir um cliente\n");
+          //   printf("Digite [3] para consultar saldo\n");
+
+          //   int entradaMenuGerente;
+          //   scanf("%d", &entradaMenuGerente);
             
 
-            if(entradaMenuGerente == 01){
-              printf("Digite o nome do cliente:\n")
-              agencia.cliente[contadorClientes]              
-            }
-          }
+            // if(entradaMenuGerente == 01){
+            //   printf("Digite o nome do cliente: \n");
+            //   agencia.clientes[contadorClientes];              
+            // }
+          //}
         }
+        else{
+            printf("Login inexistente: \n");
+          }
+
     }
 
     if(entradaUser == 2){
