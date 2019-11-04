3176dbbbbc25c41af106a5cbd4495a338e071929
arthurmeireles <48288137+arthurmeireles@users.noreply.github.com>
2019-11-03 22:33:37 -0300

    ajusta a entrada e comparações entre strings

--- main.c
+++ main.c
@@ -3,16 +3,15 @@
 
 struct Cliente{
   char nome[255];
-  char cpf[11];
+  char rg[12];
+  char cpf[12];
   float saldo;
-  int ContaCorrente;
-  int ContaESpecial;
-  char senha[10];
+  char senha[7];
 };
 
 struct Gerente{
-  int login;
-  char senha;
+  char login[6];
+  char senha[7];
 };
 
 struct Agencia{
@@ -22,60 +21,22 @@ struct Agencia{
 
 int main(void) {
 
+//instancia a agencia
   struct Agencia agencia;
-  agencia.gerente.login = 12345;
-  agencia.gerente.senha = "batata";
+  strcpy(agencia.gerente.login, "12345");
+  strcpy(agencia.gerente.senha, "batata");
 
-
-
+//inicia o contador de clientes da agencia
   int contadorClientes = 0; 
 
+// cria o ciclo da agencia
   while(1>0){
     printf("Deseja entrar como Gerente ou como Cliente? \nDigite [1] para entrar como gerente.\nDigite [2] para entrar como cliente.\n");
-
+    
+    // pega a opção de entrada
     int entradaUser;
-
     scanf("%d", &entradaUser);
 
-    if(entradaUser == 1){
-      
-      printf("Digite seu login: \n");
-      int loginEntrada;
-      scanf("%d", &loginEntrada);
-        
-        if(loginEntrada == agencia.gerente.login){
-
-          printf("Digite sua senha: \n");
-
-          char senhaEntrada[7];
-          scanf("%s", &senhaEntrada);
-
-          printf("%s", senhaEntrada);
-
-          printf("%d", strcmp(senhaEntrada, agencia.gerente.senha));
-
-
-          // if(strcmp(senhaEntrada, agencia.gerente.senha) == 0){
-          //   printf("----------Menu Gerente--------\n");
-          //   printf("Digite [1] para Cadastrar um cliente\n");
-          //   printf("Digite [2] para Excluir um cliente\n");
-          //   printf("Digite [3] para consultar saldo\n");
-
-          //   int entradaMenuGerente;
-          //   scanf("%d", &entradaMenuGerente);
-            
-
-            // if(entradaMenuGerente == 01){
-            //   printf("Digite o nome do cliente: \n");
-            //   agencia.clientes[contadorClientes];              
-            // }
-          //}
-        }
-        else{
-            printf("Login inexistente: \n");
-          }
-
-    }
 
     if(entradaUser == 2){
       printf("----------Menu Usuario--------\n");
@@ -84,7 +45,77 @@ int main(void) {
       printf("Digite [3] para consultar saldo\n");
     }
 
+    //entrada como gerente
+    if(entradaUser == 1){
+      
+      printf("Digite seu login: \n");
+      
+      char loginEntrada[6];
+      
+      scanf("%s", &*loginEntrada);
+
+      if(strcmp(loginEntrada, agencia.gerente.login) == 0){
+          
+          printf("Digite sua senha: \n");
+          char senhaEntrada[11];
+          scanf("%s", &*senhaEntrada);
+
+
+          if(strcmp(senhaEntrada, agencia.gerente.senha) == 0){
+            printf("\n----------Menu Gerente--------\n");
+            printf("Digite [1] para Cadastrar um cliente\n");
+            printf("Digite [2] para Excluir um cliente\n");
+            printf("Digite [3] para consultar saldo\n");
+            printf("Digite [4] para listar os clientes da agência\n");
+
+            int entradaMenuGerente;
+            scanf("%d", &entradaMenuGerente);
+            
+
+            if(entradaMenuGerente == 01){
+              printf("\n----------Cadastro de Clientes----------\n");
+             
+              printf("Digite o nome do cliente: \n");
+              scanf("%s", &*agencia.clientes[contadorClientes].nome);    
+
+              printf("Digite o rg do cliente: \n");
+              scanf("%s", &*agencia.clientes[contadorClientes].rg);
+
+              printf("Digite o CPF do cliente: \n");
+              scanf("%s", &*agencia.clientes[contadorClientes].cpf);
+
+              printf("Digite uma senha de até 6 digitos: \n");
+              scanf("%s", &*agencia.clientes[contadorClientes].senha);
+
+              agencia.clientes[contadorClientes].saldo = 0;
+              contadorClientes++;
+            }
+            if(entradaMenuGerente == 4){
+              printf("\n----------Lista de clientes----------\n");
+              for(int i = 0; i< contadorClientes; i++){
+                printf("Nome : %s\n", agencia.clientes[i].nome);
+                printf("CPF : %s\n", agencia.clientes[i].cpf);
+                printf("RG : %s\n", agencia.clientes[i].rg);
+                printf("Nome : %s\n", agencia.clientes[i].nome);
+                printf("-------------------------------\n");
+              }
+            }
+
+          //fecha o if de comparação da senha
+          }
+        }
+      
+        else{
+          printf("Login inexistente: \n");
+        }
+
+//fecha o if de comparação do login 
+    }
+
+
+
   }
 
+//finaliza o sistema
   return 0;
 }
